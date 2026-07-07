bbdafacaca4677694238762b87f3cf75a2f02386
gouravkamble9 <gouravkamble9@gmil.com>
2026-07-07 21:02:59 +0530

    implement search and category

--- src/Paginated.jsx
+++ src/Paginated.jsx
@@ -1,14 +1,17 @@
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import debounce from 'lodash.debounce';
 import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 function Products() {
 
-    const [searchParams,setSearchParams]=useSearchParams({skip:0,limi:4})
+    const [searchParams, setSearchParams] = useSearchParams({ skip: 0, limi: 4 })
 
-    const skip=parseInt(searchParams.get("skip") || 0)
-    const limit=parseInt(searchParams.get("limit") || 4)
-    
+    const skip = parseInt(searchParams.get("skip") || 0)
+    const limit = parseInt(searchParams.get("limit") || 4)
+    const q = searchParams.get('q') || ''
+    const category = searchParams.get('category') || ''
+
     const { data: categories } = useQuery({
         queryKey: ['categories'],
         queryFn: async () => {
@@ -19,18 +22,23 @@ function Products() {
     });
 
     const { data: products } = useQuery({
-        queryKey: ['products',limit,skip],
+        queryKey: ['products', limit, skip, q, category],
         queryFn: async () => {
-            const data = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`).then((res) => res.json());
-            return data.products;
+            let url = `https://dummyjson.com/products/search?limit=${limit}&skip=${skip}&q=${q}`
+
+            if (category) {
+                url = `https://dummyjson.com/products/category/${category}?limit=${limit}&skip=${skip} `
+            }
+            return await fetch(url).then((res) => res.json());
+
         },
-        placeholderData:keepPreviousData
+        placeholderData: keepPreviousData
     });
 
-    const handleMove=(moveCount)=>{
-        setSearchParams((prev)=>{
-                prev.set('skip',Math.max(skip+moveCount,0))
-                return prev
+    const handleMove = (moveCount) => {
+        setSearchParams((prev) => {
+            prev.set('skip', Math.max(skip + moveCount, 0))
+            return prev
         })
     }
 
@@ -46,14 +54,29 @@ function Products() {
                     <div>
                         <div className="relative mt-2 rounded-md flex items-center gap-8 mb-4">
                             <input
-                                onChange={() => {}}
+                                onChange={debounce((e) => {
+                                    setSearchParams((prev) => {
+                                        prev.set('q', e.target.value)
+                                        prev.set('skip', 0)
+                                        prev.delete('category')
+                                        return prev
+                                    })
+                                }, 1000)}
                                 type="text"
                                 name="price"
                                 id="price"
                                 className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="IPhone"
                             />
-                            <select className="border p-2" onChange={() => {}}>
+                            <select className="border p-2" onChange={(e) => {
+                                setSearchParams((prev) => {
+
+                                    prev.set('category', e.target.value)
+                                    prev.set('skip', 0)
+                                    prev.delete('q')
+                                    return prev
+                                })
+                            }}>
                                 <option>Select category</option>
                                 {categories?.map((category) => (
                                     <option key={category.slug} value={category.slug}>
@@ -65,7 +88,7 @@ function Products() {
                     </div>
 
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                        {products?.map((product) => (
+                        {products?.products?.map((product) => (
                             <div key={product.id} className="group relative">
                                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-64">
                                     <img
@@ -99,13 +122,15 @@ function Products() {
 
                     <div className="flex gap-2 mt-12">
                         <button
-                            className="bg-purple-500 px-4 py-1 text-white rounded"
-                            onClick={() => {handleMove(-limit)}}>
+                            disabled={skip < limit}
+                            className="bg-purple-500 cursor-pointer px-4 py-1 text-white rounded"
+                            onClick={() => { handleMove(-limit) }}>
                             Prev
                         </button>
                         <button
-                            className="bg-purple-500 px-4 py-1 text-white rounded"
-                            onClick={() => {handleMove(limit)}}>
+                            disabled={skip+limit>=products?.total}
+                            className="bg-purple-500 cursor-pointer px-4 py-1 text-white rounded"
+                            onClick={() => { handleMove(limit) }}>
                             Next
                         </button>
                     </div>
